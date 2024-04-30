eac92e823bdd735a3a0f56d1d26d31863c733fca
Juanique Voogt <juanique@kartoza.com>
2024-04-30 09:31:53 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Course Management
+
+Management for courses on website
+
+#### License
+
+MIT
---
+++ course_management/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Course Management",
+			"type": "module",
+			"label": _("Course Management")
+		}
+	]
