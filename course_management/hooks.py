c0f3aaa10409f13fc9d224ee4e6f6ee247e338d6
Juanique Voogt <voogt001@gmail.com>
2024-12-02 14:44:36 +0200

    first commit

--- course_management/hooks.py
+++ course_management/hooks.py
@@ -15,6 +15,7 @@ app_license = "MIT"
 
 # include js, css files in header of web template
 web_include_css = f"/assets/course_management/css/course_management.css?v={datetime.now()}"
+
 # web_include_js = "/assets/course_management/js/course_management.js"
 
 # include custom scss in every website theme (without file extension ".scss")
